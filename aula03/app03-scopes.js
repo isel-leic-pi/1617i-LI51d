0e76cc43c07ece6c616264cd2a2f77cc4737762f
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-09-27 16:38:13 +0100

    Fix lesson 03 with additional comments

--- aula03/app03-scopes.js
+++ aula03/app03-scopes.js
@@ -1,26 +1,31 @@
 function foo() {
+    console.log(y) // > undefined
     for(var x = 0; x < 5; x++){
         var y = "ola"
         console.log(x)
     }
-    z = x
+    z = x          // z variavel global
     console.log('Fim do loop')
     console.log(x)
     console.log(y)
+    console.log('------------------------------------')
 }
 
 function bar() {
-    let x, y
-    for(x = 0; x < 5; x++){
+    "use strict"
+    // console.log(x) // ERRO variable x is not defined
+    let y
+    for(let x = 0; x < 5; x++){
         y = "ola"
         console.log(x)
     }
+    // w = x          // declaração de w global não é permitida em modo "use strict"
     console.log('Fim do loop')
-    console.log(x)
+    // console.log(x) // Inacessível fora do bloco.
     console.log(y)
+    console.log('------------------------------------')
 }
-
-// console.log(y) -- INACESSIVEL for do Scope da função foo() 
 
 foo()
 bar()
+console.log(z)  // aceder à variável z global
