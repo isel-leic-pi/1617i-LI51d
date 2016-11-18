1b6409dd29f899db8beee0d13793a2cc45447b17
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-18 12:02:06 +0000

    Move Controller handling into Controller module

--- aula26-web-app-football-connect/controller/footballController.js
+++ aula26-web-app-football-connect/controller/footballController.js
@@ -5,10 +5,33 @@ const handlebars = require('handlebars')
 const FootballService = require('./../model/footballService.js')
 const httpGetAsJson = require('./../httpGetAsJson.js')
 const footService = new FootballService(httpGetAsJson)
+const url = require('url')
 
 const viewLeagueTable = handlebars.compile(fs.readFileSync('./views/leagueTable.hbs').toString())
 
-const handlers = {}
+const handlers = function(req, resp){
+        const urlInfo = url.parse(req.url, true)
+        const parts = urlInfo.pathname.split('/')
+        const endPoint = parts[parts.length -1]
+
+        if(handlers.hasOwnProperty(endPoint))
+        {    
+            handlers[endPoint](urlInfo.query, (err, content) => {
+                if(err) {
+                    resp.writeHead(500)
+                    resp.write(err.message)
+                }
+                else{
+                    resp.writeHead(200, { 'Content-Type': 'text/html' })
+                    resp.write(content)
+                }
+                resp.end() // Termina a ligação
+            })
+        } else {
+            resp.writeHead(404)
+            resp.end() // Termina a ligação
+        }
+}
 
 handlers.leagueTable = function(query, cb) {
     const id = query.id
--- aula26-web-app-football-connect/footballWebApp.js
+++ aula26-web-app-football-connect/footballWebApp.js
@@ -3,39 +3,19 @@
 const port = process.argv[2] | 3000
 const fs = require('fs')
 const connect = require('connect')
-const url = require('url')
 const footballController = require('./controller/footballController.js')
 const ecstatic = require('ecstatic')
 const favicon = require('serve-favicon')
 
-const server = connect()
-
+const server = connect() // Init pipeline
+/*
+ * Add Middlewares
+ */
 server.use(favicon(__dirname + '/public/luma.ico'));
 server.use(ecstatic({root: __dirname + '/public' }));
-
-server.use((req, resp) => {
-        const urlInfo = url.parse(req.url, true)
-        const parts = urlInfo.pathname.split('/')
-        const endPoint = parts[parts.length -1]
-
-        if(footballController.hasOwnProperty(endPoint))
-        {    
-            footballController[endPoint](urlInfo.query, (err, content) => {
-                if(err) {
-                    resp.writeHead(500)
-                    resp.write(err)
-                }
-                else{
-                    resp.writeHead(200, { 'Content-Type': 'text/html' })
-                    resp.write(content)
-                }
-                resp.end() // Termina a ligação
-            })
-        } else {
-            resp.writeHead(404)
-            resp.end() // Termina a ligação
-        }
-})
-
+server.use(footballController)
+/*
+ * Run server
+ */
 server.listen(port)
 console.log('HTTP Server running on port ' + port)
