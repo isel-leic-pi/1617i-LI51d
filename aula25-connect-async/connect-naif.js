f21ee2643f843f06852b9c37075106400c151b30
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-18 10:00:04 +0000

    Version 2 -- Functional approach using some() auxiliary method

--- aula25-connect-async/connect-naif.js
+++ aula25-connect-async/connect-naif.js
@@ -3,10 +3,14 @@
 module.exports = function() {
     const mws = []
     const pipe = function(req, resp) {
-        for(let i = 0; i < mws.length ;i++){
-        	if(resp.finished != true)
-        		mws[i](req,resp)
-        }
+        /* "Array.prototype.some
+         * some() is pretty much the same as forEach but it
+         * breaks when the callback returns true." 
+         **/
+        mws.some( middleware => {
+            middleware(req, resp)
+            return resp.finished
+        })
     }
     pipe.use = function(mw) {
         mws.push(mw)
