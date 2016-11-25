474f3fb644ebc9994499a9d7e8560e6d75d995a8
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-25 10:18:04 +0000

    Add basic structure of passport-naif: initialize() and authenticate()

---
+++ aula28-web-app-passport-naif/passport-naif.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+    'initialize': initialize,
+    'authenticate':authenticate
+}
+
+function initialize(){
+    return (req, res, next) => {
+
+    }
+}
+
+function authenticate(){
+    return (req, res, next) => {
+        
+    }
+}
