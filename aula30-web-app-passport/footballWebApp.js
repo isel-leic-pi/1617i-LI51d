c41002a82d4dbba4012971d15a3a4c0b2c0df8ac
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-30 09:54:13 +0000

    aula30 Setup

---
+++ aula30-web-app-passport/footballWebApp.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const port = process.argv[2] | 3000
+const fs = require('fs')
+const connect = require('express')
+const footballController = require('./controller/footballController.js')
+const ecstatic = require('ecstatic')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const passport = require('./passport-naif.js')
+const usersService = require('./model/usersService.js')
+
+const server = connect() // Init pipeline
+passport.use({
+    name: 'local',
+    authenticate: (req, cb) => {
+        usersService.authenticate(
+            req.query.username, 
+            req.query.password,
+            cb)
+    }
+})
+passport.deserializeUser((userId, cb) => {
+    usersService.find(userId, cb)
+})
+passport.serializeUser((user, cb) => {
+    cb(null, user.username)
+})
+
+/*
+ * Add Middlewares
+ */
+server.use(favicon(__dirname + '/public/luma.ico'));
+server.get('/', (req, res) => res.redirect('/leagues'))
+server.use(ecstatic({root: __dirname + '/public' }));
+server.use(cookieParser())
+server.use(bodyParser())
+server.use(passport.initialize())
+server.use(footballController)
+server.use('/login', passport.authenticate())
+
+
+server.use((err, req, resp, next) => {
+    resp.writeHead(500)
+    resp.write(err.message)
+    resp.end() // Termina a ligação
+})
+
+server.use((req, resp) => {
+    resp.writeHead(404)
+    resp.end() // Termina a ligação
+})
+/*
+ * Run server
+ */
+server.listen(port)
+console.log('HTTP Server running on port ' + port)
