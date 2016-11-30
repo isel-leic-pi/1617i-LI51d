fe3b99ba04dc25055aaaf434f3453bef5acc2b9e
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-11-30 14:46:56 +0000

    Replace handlebars by hbs and res.render()

--- aula31-web-app-football-express/controllers/football.js
+++ aula31-web-app-football-express/controllers/football.js
@@ -5,44 +5,36 @@ const FootballService = require('./../model/footballService.js')
 const httpGetAsJson = require('./../httpGetAsJson.js')
 const footService = new FootballService(httpGetAsJson)
 
-const handlebars = require('handlebars')
-handlebars.registerPartial(
+require('hbs').registerPartial(
     'favourites', 
     fs.readFileSync('./views/partialFavourites.hbs').toString()
 )
-const viewLeagueTable = handlebars.compile(fs.readFileSync('./views/leagueTable.hbs').toString())
-const viewLeagues = handlebars.compile(fs.readFileSync('./views/leagues.hbs').toString())
 
-const handlers = function(req, resp, next){
+const handlers = function(req, res, next){
         const parts = req.path.split('/')
         const endPoint = parts[parts.length -1]
 
         if(!handlers.hasOwnProperty(endPoint)) return next()
-        handlers[endPoint](req, (err, content) => {
-            if(err) return next(err)
-            resp.writeHead(200, { 'Content-Type': 'text/html' })
-            resp.write(content)
-            resp.end() // Termina a ligação
-        })
+        handlers[endPoint](req, res, next)
 }
 
-handlers.leagueTable = function(req, cb) {
+handlers.leagueTable = function(req, res, next) {
     const query = req.query
     const id = query.id
     footService.getLeagueTable(id, (err, league) => {
-        if(err) return cb(err)
+        if(err) return next(err)
         league.user = req.user
-        cb(null, viewLeagueTable(league))
+        res.render('leagueTable', league)
     })    
 }
 
-handlers.leagues = function(req, cb) {
+handlers.leagues = function(req, res, next) {
     const query = req.query
     footService.getLeagues((err, leagues) => {
-        if(err) return cb(err)
+        if(err) return next(err)
         leagues = leaguesWithLinks(leagues)
         leagues.user = req.user
-        cb(null, viewLeagues(leagues))
+        res.render('leagues', leagues)
     })    
 }
 
