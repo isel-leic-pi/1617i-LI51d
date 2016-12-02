b1057e466fc28afcac8e9dad379fab09fc601bda
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-12-02 16:58:02 +0000

    Fix httpGetAsJson to work with es6 Promise instead of q

--- aula33-web-app-football-promises/httpGetAsJson.js
+++ aula33-web-app-football-promises/httpGetAsJson.js
@@ -1,18 +1,18 @@
 "use strict";
 
 const http = require('http')
-const q = require('q')
 
 /**
  * Does a HTTP get request and converts the JSON result to a Javascript object.
  */
 module.exports = function(path){
-    const defer = q.defer()
-    http.get(path, (resp) => {
-        let res = ''
-        resp.on('error', err => defer.reject(err))
-        resp.on('data', chunck => res += chunck.toString())
-        resp.on('end', () => defer.resolve(JSON.parse(res)))
+    const promise = new Promise((resolve, reject) => {
+        http.get(path, (resp) => {
+            let res = ''
+            resp.on('error', err => reject(err))
+            resp.on('data', chunck => res += chunck.toString())
+            resp.on('end', () => resolve(JSON.parse(res)))
+        })
     })
-    return defer.promise
+    return promise
 }
