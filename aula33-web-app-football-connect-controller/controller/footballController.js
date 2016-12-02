8b872c557dada502d7cd483ef68e1d0d1aacf9fc
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-12-02 11:28:13 +0000

    Setup aula 33 connect-controller

---
+++ aula33-web-app-football-connect-controller/controller/footballController.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const fs = require('fs')
+const FootballService = require('./../model/footballService.js')
+const httpGetAsJson = require('./../httpGetAsJson.js')
+const footService = new FootballService(httpGetAsJson)
+
+const hbs = require('hbs')
+hbs.registerPartial(
+    'favourites', 
+    fs.readFileSync('./views/partialFavourites.hbs').toString()
+)
+
+const handlers = function(req, res, next){
+        const parts = req.path.split('/')
+        const endPoint = parts[parts.length -1]
+
+        if(!handlers.hasOwnProperty(endPoint)) return next()
+        handlers[endPoint](req, res, next)
+}
+
+handlers.leagueTable = function(req, res, next) {
+    const query = req.query
+    const id = query.id
+    footService.getLeagueTable(id, (err, league) => {
+        if(err) return next(err)
+        league.title = 'League Table'
+        league.user = req.user
+        res.render('leagueTable', league) // status 200 + res.write(...) + res.end()
+    })    
+}
+
+handlers.leagues = function(req, res, next) {
+    const query = req.query
+    footService.getLeagues((err, leagues) => {
+        if(err) return next(err)
+        leagues = leaguesWithLinks(leagues)
+        leagues.title = 'Leagues'
+        leagues.user = req.user
+        res.render('leagues', leagues) // status 200 + res.write(...) + res.end()
+    })    
+}
+
+function leaguesWithLinks(leagues) {
+    return leagues.map(item => {
+        item.leagueHref = "/leagueTable?id=" + item.id
+        return item 
+    })
+}
+
+module.exports = handlers
