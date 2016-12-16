6c3ef3be37b69f96cad9dae31b4911702021a659
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2016-12-16 11:42:56 +0000

    Add getTeam(teamId) to footballService

--- aula37-DOM-and-AJAX/model/Team.js
+++ aula37-DOM-and-AJAX/model/Team.js
@@ -1,13 +1,28 @@
 "use strict";
 
- function Team(obj) {
-    const path = obj._links.team.href.split('/')
-    this.id = path[path.length - 1]
-    this.position = obj.position
-    this.name = obj.teamName
-    this.points = obj.points
-    this.goals = obj.goals
-    this.players = []
+ function Team(obj, teamId) {
+     return teamId
+        ? initTeam(this, teamId, obj)
+        : initTeamForTable(this, obj)
  }
+
+function initTeam(self, teamId, obj) {
+    self.id = teamId
+    self.name = obj.name
+    self.code = obj.code
+    self.shortName = obj.shortName
+    self.squadMarketValue = obj.squadMarketValue 
+    self.crestUrl = obj.crestUrl
+}
+
+function initTeamForTable(self, obj) {
+    const path = obj._links.team.href.split('/')
+    self.id = path[path.length - 1]
+    self.position = obj.position
+    self.name = obj.teamName
+    self.points = obj.points
+    self.goals = obj.goals
+    self.players = []
+}
 
 module.exports = Team
